fbc56748677ab320a747adae0df1f77dfa3e0ee7
yqxia <yqxia@thoughtworks.com>
2018-03-06 14:54:05 +0800

    [yuqing]add sass config

--- client/src/components/App.js
+++ client/src/components/App.js
@@ -1,4 +1,5 @@
 import React, { Component } from 'react';
+import '../stylesheets/app.css';
 
 class App extends Component {
   render() {
--- client/test/App.test.js
+++ client/test/App.test.js
@@ -1,5 +1,5 @@
 import React from 'react';
-import App from '../src/App';
+import App from '../src/components/App';
 import {expect} from 'chai';
 import {shallow} from 'enzyme';
 
